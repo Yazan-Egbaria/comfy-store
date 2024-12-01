bb9182b71b901e4ebc42bf7c3ee6f0964a996098
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-12-01 16:51:29 +0200

    Finished the register page, only the functionalities left to go

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -6,7 +6,10 @@ const HomeLayout = () => {
       <nav>
         <h2 className="text-primary text-4xl">Comfy</h2>
       </nav>
-      <Outlet />
+
+      <div className="pagePadding">
+        <Outlet />
+      </div>
     </>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,34 @@
+import { Form, Link } from "react-router-dom";
+import FormInput from "../components/FormInput";
+import SubmitBtn from "../components/SubmitBtn";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="grid h-screen place-items-center">
+      <Form
+        method="post"
+        className="card bg-base-200 flex w-96 flex-col gap-y-4 p-8 shadow-lg"
+      >
+        <h4 className="text-center text-3xl font-bold">Register</h4>
+        <FormInput type="text" label="Username" name="username" />
+        <FormInput type="email" label="Email" name="email" />
+        <FormInput type="password" label="Password" name="password" />
+
+        <div className="mt-4">
+          <SubmitBtn text="Register" />
+        </div>
+        <p className="text-center">
+          Already a member?{" "}
+          <Link
+            to="/login"
+            className="link link-hover link-primary ml-2 capitalize"
+          >
+            Login
+          </Link>
+        </p>
+      </Form>
+    </div>
+  );
 };
 
 export default Register;
